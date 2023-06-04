2928bcd58c5434a3ed08889fb05398701a977da4
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-04 15:39:41 +0500

    Add firebase dependencies

---
+++ src/components/TasksList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useFirestore } from 'vuefire'
+import { useCollection } from 'vuefire'
+import { addDoc, collection } from 'firebase/firestore'
+
+const db = useFirestore()
+
+const tasks = useCollection(collection(db, 'tasks'))
+</script>
+
+<template>
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      <span>{{ task.text }}</span>
+    </li>
+  </ul>
+</template>
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection } from 'firebase/firestore'
+
+
+export const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAXIX84H7KsYzZHxfJWCoVGjRQFuXv9Vq8",
+  authDomain: "farenda-dab7b.firebaseapp.com",
+  projectId: "farenda-dab7b",
+  storageBucket: "farenda-dab7b.appspot.com",
+  messagingSenderId: "750334561302",
+  appId: "1:750334561302:web:2ab84ce2fde75ab5273937",
+  measurementId: "G-QXQK64Q606"
+})
