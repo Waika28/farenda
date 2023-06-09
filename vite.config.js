4ae123e54c99f5ab9026630816905dee3c266a4a
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-09 19:01:17 +0500

    Update PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,63 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: "Farenda",
+        short_name: "Farenda",
+        description: "ToDo app by Waika28",
+        lang: "en",
+        theme_color: "#B64646",
+        display: "standalone",
+        background_color: "#D9D9D9",
+        orientation: "portrait",
+        icons [
+
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
