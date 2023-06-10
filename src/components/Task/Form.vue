6e68411597090e062a5098d0f18f0b0a5e294f8d
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-10 15:38:26 +0500

    Move add task card to modal

--- src/components/Task/Form.vue
+++ src/components/Task/Form.vue
@@ -22,19 +22,31 @@ function addTask() {
 </script>
 
 <template>
-  <div class="card bg-base-200 shadow-xl">
-    <div class="card-body">
-      <div class="card-title">Add task</div>
-      <form @submit.prevent="addTask">
-        <div class="join">
-          <div class="join-item">
-            <input name="text" class="input input-bordered" type="text" v-model="text" />
+  <button class="btn btn-circle absolute bottom-4 right-4" onclick="add_task.showModal()">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21L12 3M3 12l18 0" />
+    </svg>
+  </button>
+  <dialog id="add_task" class="modal">
+    <div class="relative card bg-base-200 shadow-xl">
+      <button class="absolute right-4 top-4" onclick="add_task.close()">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </button>
+      <div class="card-body">
+        <div class="card-title">Add task</div>
+        <form @submit.prevent="addTask">
+          <div class="join">
+            <div class="join-item">
+              <input name="text" class="input input-bordered" type="text" v-model="text" />
+            </div>
+            <div class="join-item">
+              <button class="btn btn-primary">Submit</button>
+            </div>
           </div>
-          <div class="join-item">
-            <button class="btn btn-primary">Submit</button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
+  </dialog>
 </template>
