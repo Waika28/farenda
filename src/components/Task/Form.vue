f5a95ffd22616f761d2fc9dff0e1a101a6241e48
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-10 16:08:28 +0500

    Close modal when click outside

--- src/components/Task/Form.vue
+++ src/components/Task/Form.vue
@@ -28,25 +28,23 @@ function addTask() {
     </svg>
   </button>
   <dialog id="add_task" class="modal">
-    <div class="relative card bg-base-200 shadow-xl">
-      <button class="absolute right-4 top-4" onclick="add_task.close()">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-        </svg>
-      </button>
-      <div class="card-body">
-        <div class="card-title">Add task</div>
-        <form @submit.prevent="addTask">
-          <div class="join">
-            <div class="join-item">
-              <input name="text" class="input input-bordered" type="text" v-model="text" />
-            </div>
-            <div class="join-item">
-              <button class="btn btn-primary">Submit</button>
-            </div>
+    <div class="modal-box w-auto">
+      <form @submit.prevent="addTask">
+        <div class="text-xl font-semibold">
+          <label for="text">Add task</label>
+        </div>
+        <div class="join">
+          <div class="join-item">
+            <input name="text" class="input input-bordered" type="text" v-model="text" />
           </div>
-        </form>
-      </div>
+          <div class="join-item">
+            <button class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </form>
     </div>
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
   </dialog>
 </template>
