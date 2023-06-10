4c8c36ef830b16e75c64ce29c2dde30ad711c09e
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-10 17:48:38 +0500

    Resize "add task" button

--- src/components/Task/Form.vue
+++ src/components/Task/Form.vue
@@ -22,8 +22,8 @@ function addTask() {
 </script>
 
 <template>
-  <button class="btn btn-circle absolute bottom-4 right-4" onclick="add_task.showModal()">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <button class="btn btn-circle absolute bottom-4 right-4 w-14 h-14" onclick="add_task.showModal()">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21L12 3M3 12l18 0" />
     </svg>
   </button>
