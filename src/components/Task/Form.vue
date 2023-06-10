676191f1b5f5a1055d13d120c2b9e65a16b52c80
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-10 18:06:25 +0500

    Close modal after task is added

--- src/components/Task/Form.vue
+++ src/components/Task/Form.vue
@@ -17,32 +17,35 @@ function addTask() {
     isCompleted: false
   })
 
+  closeModal()
+
   text.value = ''
 }
+
+const showModal = () => add_task_modal.showModal()
+const closeModal = () => add_task_modal.close()
 </script>
 
 <template>
-  <button class="btn btn-circle absolute bottom-4 right-4 w-14 h-14" onclick="add_task.showModal()">
+  <button class="btn btn-circle absolute bottom-4 right-4 w-14 h-14" @click="showModal">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21L12 3M3 12l18 0" />
     </svg>
   </button>
-  <dialog id="add_task" class="modal">
-    <div class="modal-box w-auto">
-      <form @submit.prevent="addTask">
-        <div class="text-xl font-semibold">
-          <label for="text">Add task</label>
+  <dialog id="add_task_modal" class="modal">
+    <form class="modal-box w-auto" @submit.prevent="addTask">
+      <div class="text-xl font-semibold">
+        <label for="text">Add task</label>
+      </div>
+      <div class="join">
+        <div class="join-item">
+          <input name="text" class="input input-bordered" type="text" v-focus v-model="text" />
         </div>
-        <div class="join">
-          <div class="join-item">
-            <input name="text" class="input input-bordered" type="text" v-focus v-model="text" />
-          </div>
-          <div class="join-item">
-            <button class="btn btn-primary">Submit</button>
-          </div>
+        <div class="join-item">
+          <button class="btn btn-primary">Submit</button>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
     <form method="dialog" class="modal-backdrop">
       <button>close</button>
     </form>
