580e2eef2c310736d70fb9d339330f378da06384
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-14 19:46:59 +0500

    Add update on click for steps

--- src/components/Task/Step/Step.vue
+++ src/components/Task/Step/Step.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Timestamp } from '@firebase/firestore'
 import { toRef } from 'vue'
+import ClickToEditInput from '../ClickToEditInput.vue'
 
 const props = defineProps(['index', 'step'])
 const step = toRef(props, 'step')
@@ -13,12 +14,18 @@ function toggleCompleted() {
     completedAt: Timestamp.now()
   })
 }
+
+function updateText(newText) {
+  emit('updateStep', props.index, step.value, {
+    text: newText
+  })
+}
 </script>
 
 <template>
   <div class="flex gap-x-2 items-center">
     <input :checked="step.isCompleted" class=checkbox type="checkbox" @click="toggleCompleted">
-    <span :class="{ completed: step.isCompleted }">{{ step.text }}</span>
+    <ClickToEditInput :class="{ completed: step.isCompleted }" :value="step.text" @submit="updateText" />
     <button class="btn btn-circle btn-sm" @click="emit('deleteStep', props.index)">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
